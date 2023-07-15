e935404eefc17e90ffa4a4beaeb060f9d2797c17
jhanzeb.khatri1561@gmail.com <jhanzeb.khatri1561@gmail.com>
2023-07-15 19:03:35 +0500

    class-9

---
+++ class 9.dart
@@ -0,0 +1,12 @@
+void main () {
+var age = 45;
+String result = age >= 18 ? "Allow" : "Not Allow";
+print(result);  
+}
+
+
+// sumOfTwoNumbers (num1, num2) => num1 + num2;
+
+// sumOfTwoNumbers (num1, num2){
+//   return num1 + num2;
+// }
